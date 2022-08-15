e69f8c1e9ae300aabd78afa8255c6070f8185e44
a.karikov@powwwer.io <a.karikov@powwwer.io>
2022-08-15 13:37:23 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,87 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="app">
+    <form class="form" @submit.prevent>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Описание"
+      />
+      <button class="button" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-bind:key="post.id" v-for="post in posts">
+      <div><strong>Название </strong>{{ post.title }}</div>
+      <div><strong>Описание </strong>{{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Описание поста" },
+        { id: 2, title: "JavaScript", body: "Описание поста" },
+        { id: 3, title: "JavaScript", body: "Описание поста" },
+      ],
+      title: "",
+      body: "",
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      console.log(newPost);
+      this.posts.push(newPost);
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
+.post {
+  padding: 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.form {
+  display: flex;
+  flex-direction: column;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.button {
+  align-self: flex-end;
+  margin-top: 15px;
+}
+
+.input {
+  width: 100%;
+  padding: 10px 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
+}
+
+.app {
+  padding: 20px;
 }
 </style>
